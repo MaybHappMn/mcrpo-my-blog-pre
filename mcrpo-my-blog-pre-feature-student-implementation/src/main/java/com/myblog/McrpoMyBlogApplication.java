d951d0a74b80d18a6d58adb0c02486028b38023c
Magomed <maga200043@gmail.com>
2026-03-13 20:59:18 +0300

    Проделанная работа

---
+++ mcrpo-my-blog-pre-feature-student-implementation/src/main/java/com/myblog/McrpoMyBlogApplication.java
@@ -0,0 +1,12 @@
+package com.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McrpoMyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McrpoMyBlogApplication.class, args);
+    }
+}
